4c38bc89f1e8793440cd1adafcf8650194481cb2
Michael K <1747304+tsal@users.noreply.github.com>
2022-10-27 12:54:17 -0400

    chore: Clean-up from reverted work

--- src/arma/commands/server_manager.rs
+++ src/arma/commands/server_manager.rs
@@ -1,13 +1,10 @@
 use std::fmt::{Debug, Display, Formatter};
-use serenity::builder::CreateApplicationCommand;
-use serenity::model::application::interaction::Interaction;
-use serenity::model::prelude::GuildId;
-use serenity::model::prelude::interaction::application_command::ApplicationCommandInteraction;
-use serenity::model::prelude::interaction::InteractionResponseType;
-use serenity::prelude::Context;
-use sqlx::SqlitePool;
-use crate::ArmaServerDescriptor;
-use crate::commands::steam::{GetServersAtAddressResponse, SteamResponse};
+// use serenity::builder::CreateApplicationCommand;
+// use serenity::model::prelude::interaction::application_command::ApplicationCommandInteraction;
+// use serenity::prelude::Context;
+// use sqlx::SqlitePool;
+// use crate::arma::ArmaServerDescriptor;
+// use crate::commands::steam::{GetServersAtAddressResponse, SteamResponse};
 
 pub struct ServerCommandError {
     pub message: String
@@ -27,53 +24,53 @@ impl Display for ServerCommandError {
 
 impl std::error::Error for ServerCommandError {}
 
-pub(crate) struct ServerManagerCommand {
-    database_pool: &'static SqlitePool,
-}
+// pub(crate) struct ServerManagerCommand {
+//     database_pool: &'static SqlitePool,
+// }
 
-impl ServerManagerCommand {
-    pub fn new(database_pool: &'static SqlitePool) -> Self {
-        Self {
-            database_pool
-        }
-    }
+// impl ServerManagerCommand {
+//     pub fn new(database_pool: &'static SqlitePool) -> Self {
+//         Self {
+//             database_pool
+//         }
+//     }
 
-    pub async fn handle(&self, ctx: &Context, command: &ApplicationCommandInteraction, guild_configuration: Option<ArmaServerDescriptor>) -> Result<(), ServerCommandError> {
-        let sub_command = command.data.options.get(0).unwrap();
-        match sub_command.name.as_str() {
-            "query" => { self.handle_server_query(command, guild_configuration).await },
-            _ => { Err(ServerCommandError { message: "not implemented".to_string() }) }
-        }
-    }
+    // pub async fn handle(&self, ctx: &Context, command: &ApplicationCommandInteraction, guild_configuration: Option<ArmaServerDescriptor>) -> Result<(), ServerCommandError> {
+    //     let sub_command = command.data.options.get(0).unwrap();
+    //     match sub_command.name.as_str() {
+    //         "query" => { self.handle_server_query(command, guild_configuration).await },
+    //         _ => { Err(ServerCommandError { message: "not implemented".to_string() }) }
+    //     }
+    // }
+    //
+    // pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
+    //     command
+    //         .name("server")
+    //         .description("Server management commands.")
+    // }
 
-    pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
-        command
-            .name("server")
-            .description("Server management commands.")
-    }
 
+    // async fn handle_server_query(&self, command: &ApplicationCommandInteraction, guild_configuration: Option<ArmaServerDescriptor>) -> Result<(), ServerCommandError> {
+    //     let guild_configuration = guild_configuration.ok_or(ServerCommandError { message: "no configuration found for this guild".to_string() })?;
+    //     let steam_response =
+    //         Self::fetch_servers_at_address(&guild_configuration).await;
+    //     match steam_response {
+    //         Ok(response) =>  {
+    //             Ok(())
+    //         }
+    //         Err(why) => {
+    //             Err(ServerCommandError {
+    //                 message: format!("Steam error fetching server query: {}", why.to_string())
+    //             })
+    //         }
+    //     }
+    //
+    // }
 
-    async fn handle_server_query(&self, command: &ApplicationCommandInteraction, guild_configuration: Option<ArmaServerDescriptor>) -> Result<(), ServerCommandError> {
-        let guild_configuration = guild_configuration.ok_or(ServerCommandError { message: "no configuration found for this guild".to_string() })?;
-        let steam_response =
-            Self::fetch_servers_at_address(&guild_configuration).await;
-        match steam_response {
-            Ok(response) =>  {
-                Ok(())
-            }
-            Err(why) => {
-                Err(ServerCommandError {
-                    message: format!("Steam error fetching server query: {}", why.to_string())
-                })
-            }
-        }
-
-    }
-
-    async fn fetch_servers_at_address(arma_configuration: &ArmaServerDescriptor) -> Result<SteamResponse<GetServersAtAddressResponse>, reqwest::Error> {
-        reqwest::get(
-            format!("https://api.steampowered.com/ISteamApps/GetServersAtAddress/v0001?addr={}", arma_configuration.arma_server_host))
-            .await?
-            .json::<SteamResponse<GetServersAtAddressResponse>>().await
-    }
-}
+    // async fn fetch_servers_at_address(arma_configuration: &ArmaServerDescriptor) -> Result<SteamResponse<GetServersAtAddressResponse>, reqwest::Error> {
+    //     reqwest::get(
+    //         format!("https://api.steampowered.com/ISteamApps/GetServersAtAddress/v0001?addr={}", arma_configuration.arma_server_host))
+    //         .await?
+    //         .json::<SteamResponse<GetServersAtAddressResponse>>().await
+    // }
+// }
--- src/arma/mod.rs
+++ src/arma/mod.rs
@@ -1,9 +1,41 @@
-pub mod bot;
+// pub mod bot;
 pub mod config;
 pub mod commands;
 
 pub mod prelude {
-    pub use crate::arma::bot::Manager;
-    pub use crate::arma::bot::ArmaServerDescriptor;
+    // pub use crate::arma::bot::Manager;
+    // pub use crate::arma::bot::ArmaServerDescriptor;
 }
 
+use serenity::model::id::GuildId;
+use std::env;
+
+#[derive(Clone)]
+pub struct ArmaDiscordConfiguration {
+    pub steam_api_key: String,
+    pub arma_server_host: String,
+    pub discord_guild_id: GuildId,
+}
+
+impl ArmaDiscordConfiguration {
+    // TODO: Use SQL and inject the info at start-up somehow, creating a vector of this info from
+    // TODO: the state, ready and guild data
+    pub fn new() -> Option<ArmaDiscordConfiguration> {
+        // No point if we can't even hit the API
+        if let Some(steam_api_key) = env::var("STEAM_API_KEY").ok() {
+            // Or don't know what to request
+            if let Some(arma_server_host) = env::var("ARMA_HOST_STRING").ok() {
+                // And haven't figured out a guild that manages this server ...
+                if let Some(guild_id_raw) = env::var("GUILD_ID").ok()?.parse().ok() {
+                    return Some(ArmaDiscordConfiguration{
+                        steam_api_key,
+                        arma_server_host,
+                        discord_guild_id: GuildId(guild_id_raw),
+                    })
+
+                }
+            }
+        }
+        None
+    }
+}
