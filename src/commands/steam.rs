b676d6ad85f557487b7127ab93332ce6ea335e71
Michael K <1747304+tsal@users.noreply.github.com>
2023-01-04 20:17:00 -0500

    feat: Add support for comma-separated server list

--- src/commands/steam.rs
+++ src/commands/steam.rs
@@ -1,31 +1,55 @@
+use std::time::Duration;
 use serde::Deserialize;
 use serenity::model::application::interaction::application_command::CommandDataOption;
 use serenity::utils::MessageBuilder;
 use serenity::builder::CreateApplicationCommand;
+use tokio::time::sleep;
 use crate::arma::ArmaDiscordConfiguration;
 
 pub struct ServerQueryCommand;
 
 impl ServerQueryCommand {
     pub async fn run(_options: &[CommandDataOption], arma_configuration: &ArmaDiscordConfiguration) -> String {
-        match Self::fetch_steam_data(arma_configuration).await {
-            Ok(steam_response) => MessageBuilder::new()
-                .push_bold_line("Steam Response:")
-                .push_codeblock_safe(format!("{:#?}", steam_response), Some("json"))
-                .build(),
-            Err(why) => why.to_string(),
+        let mut response = MessageBuilder::new();
+        if Self::has_multiple_servers(arma_configuration) {
+            response.push_bold_line("Steam Response:");
+            for server in arma_configuration.arma_server_host.split(',') {
+                match Self::fetch_steam_data(server).await {
+                    Ok(steam_response) => response
+                        .push_bold_line("Server Details:")
+                        .push_codeblock_safe(format!("{:#?}", steam_response), Some("json")),
+                    Err(why) => response.push_line(format!("Error grabbing details for {}: {}", server, why.to_string())),
+                };
+                let _ = sleep(Duration::from_millis(50));
+            }
+        } else {
+            match Self::fetch_steam_data(&arma_configuration.arma_server_host).await {
+                Ok(steam_response) => response
+                    .push_bold_line("Steam Response:")
+                    .push_bold_line("Server Details:")
+                    .push_codeblock_safe(format!("{:#?}", steam_response), Some("json")),
+                Err(why) => response
+                    .push_line(format!("Error grabbing details for {}: {}", &arma_configuration.arma_server_host, why)),
+            };
         }
+        response.build()
     }
     pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
         command
             .name("query")
             .description("Query the server to list all running instances.")
     }
-    async fn fetch_steam_data(arma_configuration: &ArmaDiscordConfiguration) -> Result<SteamResponse<GetServersAtAddressResponse>, reqwest::Error> {
+    async fn fetch_steam_data(server_details: &str) -> Result<SteamResponse<GetServersAtAddressResponse>, reqwest::Error> {
         reqwest::get(
-            format!("https://api.steampowered.com/ISteamApps/GetServersAtAddress/v0001?addr={}", arma_configuration.arma_server_host))
+            format!("https://api.steampowered.com/ISteamApps/GetServersAtAddress/v0001?addr={}", server_details))
             .await?
             .json::<SteamResponse<GetServersAtAddressResponse>>().await
+    }
+
+    // has_multiple_servers takes an ArmaDiscordConfiguration and returns true if there are multiple servers in the arma_server_host property.
+    // this is used to determine whether multiple calls should be made for the status or just one.
+    fn has_multiple_servers(arma_configuration: &ArmaDiscordConfiguration) -> bool {
+        arma_configuration.arma_server_host.contains(',')
     }
 }
 
@@ -33,42 +57,79 @@ pub struct ServerStatusCommand;
 
 impl ServerStatusCommand {
     pub async fn run(_options: &[CommandDataOption], arma_configuration: &ArmaDiscordConfiguration) -> String {
-        match Self::fetch_steam_data(arma_configuration).await {
-            Ok(steam_response) => {
-                if let Some(servers) = steam_response.response.servers {
-                    if let Some(server) = servers.first() {
-                        return MessageBuilder::new()
-                            .push_line("Sure!")
-                            .push_bold_line(format!("Server Status for {}:", server.name))
-                            .push_bold("Map: ")
-                            .push_line(format!("{}", server.map))
-                            .push_bold("Players: ")
-                            .push_line(format!("{}/{}", server.players, server.max_players))
-                            .push_bold("Connect: ")
-                            .push_line(format!("steam://connect/{}", arma_configuration.arma_server_host))
-                            .build()
-                    }
-                }
-                "no server found or the server is down, sorry!".to_string()
-            },
-            Err(why) => why.to_string(),
+        let mut response = MessageBuilder::new();
+        if Self::has_multiple_servers(arma_configuration) {
+            response
+                .push_line("Sure!");
+            for server in arma_configuration.arma_server_host.split(',') {
+                match Self::fetch_steam_data(&arma_configuration.steam_api_key, server).await {
+                    Ok(steam_response) => {
+                        if let Some(servers) = steam_response.response.servers {
+                            if let Some(server) = servers.first() {
+                                Self::push_server_details(&mut response, server);
+                            };
+                        } else {
+                            response.push_line(format!("no server found at {} or the server is down, sorry!", &server));
+                        };
+                    },
+                    Err(why) => {
+                        response.push_line(format!("Error grabbing details for {}: {}", server, why.to_string()));
+                    },
+                };
+                let _ = sleep(Duration::from_millis(50));
+            };
+        } else {
+            match Self::fetch_steam_data(&arma_configuration.steam_api_key, &arma_configuration.arma_server_host).await {
+                Ok(steam_response) => {
+                    if let Some(servers) = steam_response.response.servers {
+                        if let Some(server) = servers.first() {
+                            response
+                                .push_line("Sure!");
+                            Self::push_server_details(&mut response, server);
+                        };
+                    } else {
+                        response.push_line(format!("no server found at {} or the server is down, sorry!", &arma_configuration.arma_server_host));
+                    };
+                },
+                Err(why) => {
+                    response
+                        .push_line(format!("Error grabbing details for {}: {}", &arma_configuration.arma_server_host, why));
+                },
+            };
+
         }
+        response.build()
     }
     pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
         command
             .name("status")
             .description("Query the server to list all running instances.")
     }
-    async fn fetch_steam_data(arma_configuration: &ArmaDiscordConfiguration) -> Result<SteamResponse<GetServerListResponse>, reqwest::Error> {
+    async fn fetch_steam_data(api_key: &str, server_details: &str) -> Result<SteamResponse<GetServerListResponse>, reqwest::Error> {
         reqwest::get(
             format!(
                 "https://api.steampowered.com/IGameServersService/GetServerList/v1?key={}&filter=addr\\{}",
-                arma_configuration.steam_api_key,
-                arma_configuration.arma_server_host))
+                api_key,
+                server_details))
             .await?
             .json::<SteamResponse<GetServerListResponse>>().await
     }
+    // has_multiple_servers takes an ArmaDiscordConfiguration and returns true if there are multiple servers in the arma_server_host property.
+    // this is used to determine whether multiple calls should be made for the status or just one.
+    fn has_multiple_servers(arma_configuration: &ArmaDiscordConfiguration) -> bool {
+        arma_configuration.arma_server_host.contains(',')
+    }
 
+    fn push_server_details(response: &mut MessageBuilder, server: &SteamServer) {
+        response
+            .push_bold_line(format!("Server Status for {}:", server.name))
+            .push_bold("Map: ")
+            .push_line(format!("{}", server.map))
+            .push_bold("Players: ")
+            .push_line(format!("{}/{}", server.players, server.max_players))
+            .push_bold("Connect: ")
+            .push_line(format!("steam://connect/{}:{}", server.addr, server.gameport));
+    }
 }
 
 // Steam structures
